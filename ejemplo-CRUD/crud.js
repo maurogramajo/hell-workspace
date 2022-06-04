ed8d425c47d7450534b7869fb36fe824b9f93c10
maurogramajo <grama_mauro@live.com.ar>
2022-06-04 18:54:42 -0300

    Creo el archivo .js de la practica de CRUD

---
+++ ejemplo-CRUD/crud.js
@@ -0,0 +1,7 @@
+/*
+ * Clase 30 programacion - del 5 de junio del 2022
+ *
+ * Usando arrays armamos una "base de datos" y aprendimos CRUD (Create Read Update Delete)
+ */
+
+console.info('Hola mundo!');
