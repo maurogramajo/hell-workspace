54e3968b878a3f244aba8828edf946165cc40cde
maurogramajo <grama_mauro@live.com.ar>
2022-06-04 20:32:40 -0300

    Agrego metodos insert y read con su algoritmo de prueba

--- ejemplo-CRUD/README.md
+++ ejemplo-CRUD/README.md
@@ -1,1 +1,7 @@
 #Practica de CRUD (Create Read Update Delete)
+
+#Metodo insert
+Agrega un elemento producto a la lista de productos
+
+#Metodo read
+Busca todos los elementos de la lista productos que coincidan con el nombre o precio enviados como parametro
--- ejemplo-CRUD/crud.js
+++ ejemplo-CRUD/crud.js
@@ -4,4 +4,45 @@
  * Usando arrays armamos una "base de datos" y aprendimos CRUD (Create Read Update Delete)
  */
 
-console.info('Hola mundo!');
+var dbProductos = {
+	productos: []
+};
+
+dbProductos.insert = function(producto) {
+	producto._id = Math.random() + '-' + Math.random();
+	this.productos.push(producto);
+}
+
+dbProductos.read = function(query) {
+	var result = [];
+	if (!query) {
+		return this.productos;
+	}
+
+	for(var i = 0; i < this.productos.length; i++) {
+		var producto = this.productos[i];
+
+		if(query.name) {
+			if(producto.name === query.name) result.push(producto);
+		}
+		if(query.price) {
+			if(producto.price === query.price) result.push(producto);
+		}
+	}
+
+	return result;
+}
+
+//Algoritmo de prueba de las funciones
+
+for(var i = 1; i< 13; i ++) {
+	dbProductos.insert({
+		name: 'Iphone ' + i,
+		price: Math.round(Math.random() * 2000)
+	});
+}
+
+console.info(dbProductos);
+
+console.info('Buscando Iphone 10');
+dbProductos.read({name: 'Iphone 10'});
